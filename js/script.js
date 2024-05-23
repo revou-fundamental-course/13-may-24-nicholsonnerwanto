752bee0a461b3ff17bb22a97e08c3d770ba4870d
Nicholson Nerwanto <nicholson.nrwnto@gmail.com>
2024-05-23 20:26:35 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -18,10 +18,10 @@ function reset() {
 }
 
 function luasSegitiga() { 
-    let alas = document.getElementById('alas-input').value;
-    let tinggi = document.getElementById('tinggi-input').value;
+    let alas = parseFloat(document.getElementById('alas-input').value);
+    let tinggi = parseFloat(document.getElementById('tinggi-input').value);
     if(alas!='' && tinggi != ''){
-        luas = 0.5*alas*tinggi;
+        luas = 0.5*(alas*tinggi);
         document.getElementById('hasil-luas').innerHTML = 'L = <sup>1</sup>/<sub>2</sub> x a x t <br> L = <sup>1</sup>/<sub>2</sub> x '+ alas +' x '+ tinggi +'<br> L = <b> '+ luas +' <b>';
     
     } else {
@@ -30,12 +30,12 @@ function luasSegitiga() {
 }
 
 function kelilingSegitiga() {    
-    let sisi1 = document.getElementById('sisi1-input').value;
-    let sisi2 = document.getElementById('sisi2-input').value;
-    let sisi3 = document.getElementById('sisi3-input').value;
+    let sisi1 = parseFloat(document.getElementById('sisi1-input').value);
+    let sisi2 = parseFloat(document.getElementById('sisi2-input').value);
+    let sisi3 = parseFloat(document.getElementById('sisi3-input').value);
 
     if(sisi1!='' && sisi2 !='' && sisi3!=''){        
-        keliling = sisi1 + sisi2 + sisi3;
+        keliling = sisi1+sisi2+sisi3
         document.getElementById('hasil-keliling').innerHTML = 'K = S1 (AB) x S2 (BC) x S3 (CA)<br>K = '+ sisi1 +' + '+ sisi2 +' + '+ sisi3 +' <br>K = <b>'+ keliling +'<b>';
     
     } else {
